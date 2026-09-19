4eb5a7cf8e7b9ea0d2e9700ae5aa423fe2e55d7c
Premkumar012 <premkumard021@gmail.com>
2026-09-19 22:27:31 +0530

    Prepare Flask app for Render

--- app.py
+++ app.py
@@ -15,20 +15,16 @@ app.secret_key = "ssm_secret_key"
 
 
 def get_db_connection():
-
-    print("HOST =", os.getenv("MYSQLHOST"))
-    print("PORT =", os.getenv("MYSQLPORT"))
-    print("USER =", os.getenv("MYSQLUSER"))
-    print("DB =", os.getenv("MYSQLDATABASE"))
-
     conn = mysql.connector.connect(
         host=os.getenv("MYSQLHOST"),
+        port=int(os.getenv("MYSQLPORT")),
         user=os.getenv("MYSQLUSER"),
         password=os.getenv("MYSQLPASSWORD"),
         database=os.getenv("MYSQLDATABASE"),
-        port=int(os.getenv("MYSQLPORT"))
+        ssl_ca=os.getenv("MYSQL_SSL_CA"),
+        ssl_verify_cert=True,
+        ssl_verify_identity=True
     )
-
     return conn
 
 
